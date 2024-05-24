b33e6514e5b3ce89ad0b780b6d6c41c439846878
Aurélien Corniglion <aurelien.c1311@hotmail.com>
2024-05-24 12:34:02 +0200

    hehe

--- animation.js
+++ animation.js
@@ -1,15 +1,3 @@
-// Détecter la langue du navigateur
-var langueNavigateur = navigator.language || navigator.userLanguage;
-
-// Rediriger ou charger dynamiquement le contenu pour la langue détectée
-if (langueNavigateur.startsWith("fr")) {
-    window.location.href = "index.html"; // Redirection vers la version française
-} else if (langueNavigateur.startsWith("es")) {
-    window.location.href = "index_es.html"; // Redirection vers la version espagnole
-} else {
-    // Chargez la version par défaut du site si aucune langue n'est détectée
-}
-
 // JavaScript pour charger et appliquer les traductions
 fetch('fr.json') // Charger le fichier JSON de traduction approprié
   .then(response => response.json())
